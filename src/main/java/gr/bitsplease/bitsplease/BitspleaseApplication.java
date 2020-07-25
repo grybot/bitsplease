64a683725ac4b0951ae97e82c3a157bc38e38627
grybot <a.grympogiannis@gmail.com>
2020-07-25 12:34:34 +0300

    First commit

---
+++ src/main/java/gr/bitsplease/bitsplease/BitspleaseApplication.java
@@ -0,0 +1,13 @@
+package gr.bitsplease.bitsplease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitspleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitspleaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/bitsplease/bitsplease/BitspleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.bitsplease.bitsplease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitspleaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

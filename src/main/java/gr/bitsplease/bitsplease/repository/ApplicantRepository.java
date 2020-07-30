34d661aef367c87ef17020947a0f55d1ef612870
grybot <a.grympogiannis@gmail.com>
2020-07-30 19:15:30 +0300

    aaa

--- src/main/java/gr/bitsplease/bitsplease/repository/ApplicantRepository.java
+++ src/main/java/gr/bitsplease/bitsplease/repository/ApplicantRepository.java
@@ -1,9 +1,25 @@
 package gr.bitsplease.bitsplease.repository;
 
+import gr.bitsplease.bitsplease.dto.Reporter;
 import gr.bitsplease.bitsplease.models.Applicant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ApplicantRepository extends JpaRepository<Applicant, Integer> {
+    List<Applicant> findByFirstName(String firstName);
+
+    List<Applicant> findByRegion(String region);
+
+    @Query(nativeQuery =true, value=
+            " SELECT 		TOP(5)	 COUNT (skillsId) Freq , Skills.name Name " +
+                    " FROM ApplicantSkills INNER JOIN Skills ON ApplicantSkills.skills_skillsId= Skills.skillsId" +
+                    " GROUP BY skills_skillsId,Skills.name ORDER BY Freq Desc")
+
+    List<Reporter> findOffered();
+
+
 }
--- src/main/java/gr/bitsplease/bitsplease/repository/JobOfferRepository.java
+++ src/main/java/gr/bitsplease/bitsplease/repository/JobOfferRepository.java
@@ -1,9 +1,34 @@
 package gr.bitsplease.bitsplease.repository;
 
+import gr.bitsplease.bitsplease.dto.ReportNotMatched;
+import gr.bitsplease.bitsplease.dto.Reporter;
 import gr.bitsplease.bitsplease.models.JobOffer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface JobOfferRepository extends JpaRepository<JobOffer, Integer> {
+    List<JobOffer> findByCompanyName(String companyName);
+    List<JobOffer> findByRegion(String region);
+
+    @Query(nativeQuery =true, value=
+            " SELECT 		TOP(20)	 COUNT (skillsId) Freq , Skills.name Name " +
+                    " FROM JobOfferSkills INNER JOIN Skills ON JobOfferSkills.skills_skillsId= Skills.skillsId" +
+                    " GROUP BY skills_skillsId,Skills.name ORDER BY Freq Desc")
+
+    List<Reporter> findRequested();
+
+    @Query(nativeQuery = true , value =
+
+            " Select  skills_skillsId  Name,Skills.name From " +
+                    " JobOfferSkills  Inner join Skills ON skills_skillsId = Skills.skillsId " +
+                    " where " +
+                    " skills_skillsId not in ( " +
+                    " select skills_skillsId from ApplicantSkills)" )
+
+    List<ReportNotMatched> getNotMatchedByApplicant();
+
 }
